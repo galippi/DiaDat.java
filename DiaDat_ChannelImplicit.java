3978e606ef373c4d136fcc9e6161570c4fce8565
Gabor Liptak <liptakgabor@freemail.hu>
2021-10-17 08:33:36 +0200

    In case of implicit channel, the name type is optional

--- DiaDat_ChannelImplicit.java
+++ DiaDat_ChannelImplicit.java
@@ -4,6 +4,8 @@ public class DiaDat_ChannelImplicit extends DiaDat_ChannelBase
 {
     public DiaDat_ChannelImplicit(DiaDat_DataFileBase _parent, ChannelData chData) throws Exception
     {
+        if (chData.dataType.isEmpty())
+            chData.dataType = "REAL64"; // for implicit channel the dataType is is not mandatory
         set(_parent, chData);
     }
 
